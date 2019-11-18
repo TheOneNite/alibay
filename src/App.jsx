4e1ec4f63aef7600647dac3e5f6c0c551e7bc329
cayling <52750091+cayling@users.noreply.github.com>
2019-11-18 11:02:13 -0500

    installed react-router-dom, created store file, and set up the react wrappers

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
 class App extends Component {
-    render = () => {
-        return "Hello world!"
-    }
+  render = () => {
+    return "Hello world!";
+  };
 }
 
-
-export default App
+export default App;
