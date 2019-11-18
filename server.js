f7b3c385304df6fe12426d8809b0d5c217ad1122
TheOneNite <travisrilea@gmail.com>
2019-11-18 11:14:41 -0500

    db setup

--- server.js
+++ server.js
@@ -1,19 +1,44 @@
-let express = require('express')
-let app = express()
-let reloadMagic = require('./reload-magic.js')
+//setup of express library
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
 
-reloadMagic(app)
+//setup of multer lib
+const multer = require("multer");
+const upload = multer();
 
-app.use('/', express.static('build')); // Needed for the HTML and JS files
-app.use('/', express.static('public')); // Needed for local assets
+//setup and initialzie mongo db
+const mongoClient = require("mongodb").MongoClient;
+const dbCredientials = require("./custom_modules/mongo/dbURI.js");
+
+let mongoDb = undefined;
+mongoClient.connect(dbCredientials, (err, dbRef) => {
+  if (err) {
+    console.log(err);
+  }
+  mongoDb = dbRef;
+  console.log("-----------database initialized-----------");
+});
+
+//boilerplate
+let reloadMagic = require("./reload-magic.js");
+reloadMagic(app);
+
+app.use("/", express.static("build")); // Needed for the HTML and JS files
+app.use("/", express.static("public")); // Needed for local assets
 
 // Your endpoints go after this line
 
+app.get("/items");
+
 // Your endpoints go before this line
 
-app.all('/*', (req, res, next) => { // needed for react router
-    res.sendFile(__dirname + '/build/index.html');
-})
+app.all("/*", (req, res, next) => {
+  // needed for react router
+  res.sendFile(__dirname + "/build/index.html");
+});
 
-
-app.listen(4000, '0.0.0.0', () => { console.log("Server running on port 4000") })
+app.listen(4000, "0.0.0.0", () => {
+  console.log("Server running on port 4000");
+});
