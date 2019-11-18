a50623f97feb3e6c2e0638b918fd0d3e7c230ddc
li li <lilianelily1001@gmail.com>
2019-11-18 17:49:55 -0500

    search

--- src/Store.js
+++ src/Store.js
@@ -6,11 +6,14 @@ let reducer = (state, action) => {
   if (action.type === "signout") {
     return { ...state, loggedIn: false, currentUser: "" };
   }
+  if (action.type === "displayItems") {
+    return { ...state, displayedItems: action.items}
+  }
   return state;
 };
 const store = createStore(
   reducer,
-  { currentUser: "", loggedIn: false },
+  { currentUser: "", loggedIn: false, displayedItems:undefined},
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 export default store;
