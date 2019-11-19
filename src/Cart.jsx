6b74e1e8a12bbb602a465cc3aaa7c0f9912031e2
cayling <52750091+cayling@users.noreply.github.com>
2019-11-19 13:19:40 -0500

    adding stripe checkout button

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -2,6 +2,7 @@ import { connect } from "react-redux";
 import React, { Component } from "react";
 import ItemSearch from "./ItemSearch.jsx";
 import styled from "styled-components";
+import StripeCheckout from "react-stripe-checkout";
 
 const SearchDisplay = styled.div`
   display: flex;
@@ -36,16 +37,21 @@ class UnconnecteCart extends Component {
   };
   render = () => {
     return (
-      <SearchDisplay>
-        {this.state.displayItems.map(item => {
-          //display items
-          return (
-            <div>
-              <ItemSearch key={item.itemId} item={item} />
-            </div>
-          );
-        })}
-      </SearchDisplay>
+      <>
+        <SearchDisplay>
+          {this.state.displayItems.map(item => {
+            //display items
+            return (
+              <div>
+                <ItemSearch key={item.itemId} item={item} />
+              </div>
+            );
+          })}
+        </SearchDisplay>
+        <StripeCheckout>
+          <button>Checkout</button>
+        </StripeCheckout>
+      </>
     );
   };
 }
