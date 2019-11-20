3d87e76c91f29163fab6d51a4ddfa1a569aa9f38
li li <lilianelily1001@gmail.com>
2019-11-20 14:11:01 -0500

    first commit

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { withRouter } from "react-router-dom";
-
+//new branch
 class unconnectedSignup extends Component {
   constructor(props) {
     super(props);
